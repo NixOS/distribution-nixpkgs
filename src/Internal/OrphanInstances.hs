f772999307a71dc78cce0a6c72bb144486b7eeff
Kosyrev Serge <serge.kosyrev@iohk.io>
2018-02-27 00:52:43 +0300

    ghc 8.4 compat:  avoid clashing with the instances provided by Cabal 2

--- src/Internal/OrphanInstances.hs
+++ src/Internal/OrphanInstances.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Internal.OrphanInstances ( ) where
@@ -5,6 +6,9 @@ module Internal.OrphanInstances ( ) where
 import Control.DeepSeq
 import Distribution.System
 
+-- This is needed for GHC 8.4+, due to new Cabal
+#if !MIN_VERSION_base(4,11,0)
 instance NFData Arch
 instance NFData OS
 instance NFData Platform
+#endif
